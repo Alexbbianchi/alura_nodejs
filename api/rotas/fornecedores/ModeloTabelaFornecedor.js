014150dcf3803a6d4665d0311c2ea142cdfbc715
Alexbbianchi <alexbbianchi@gmail.com>
2021-08-14 17:56:05 -0300

    Iniciado curso 03

---
+++ api/rotas/fornecedores/ModeloTabelaFornecedor.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const intancia = require('../../banco-de-dados');
+const colunas = {
+    empresa: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    categoria: {
+        type: Sequelize.ENUM('ração', 'brinquedos'),
+        allowNull: false
+    }
+}
+
+const opcoes = {
+    freezeTableName: true,
+    tableName: 'fornecedores',
+    timestamps: true,
+    createdAt: 'dataCriacao',
+    updatedAt: 'dataAtualizacao',
+    version: 'versao'
+}
+
+module.exports = intancia.define('fornecedor', colunas, opcoes);
+
+//mysql -u root -h 127.0.0.1 -p
